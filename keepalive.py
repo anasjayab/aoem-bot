c6e4422270c1ad3705d3a7c0a43216880e566b43
anasjayab <anas.jayab2004@icloud.com>
2025-08-09 15:49:40 +0300

    Delete keepalive.py

--- keepalive.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from threading import Thread
-app = Flask(__name__)
-@app.get("/") 
-def home(): return "AoE:M Bot is running."
-def run(): app.run(host="0.0.0.0", port=8080)
-def keepalive(): Thread(target=run, daemon=True).start()
